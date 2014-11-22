3b0ca5e721a0463bcdf2a1e61eee9dc5788c97d0
aliu1032 <april_liu@yahoo.com>
2014-11-22 06:24:31 -0800

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,10 +1,12 @@
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
+    ## set value
     set <- function(y) {
         x <<-y
         m <<- NULL
     }
     get <- function () x
+    ## set inverse matrix value
     set_inv_matrix <- function(matrix) m <<- inv_matrix
     get_inv_matrix <- function() m
     
